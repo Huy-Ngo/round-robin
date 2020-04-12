7e9ec6e002871d0e66c64dcf5173aae4a8edcfe4
Huy-Ngo <duchuy29092000@gmail.com>
2020-04-12 21:15:38 +0700

    Rename blueprint

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -12,9 +12,8 @@ def index():
     schedulers = db.excecute(
         """SELECT id, scheduler_name, elapsed time
         FROM scheduler
-        ORDER BY id
-        """).fetchall()
-    return render_template('index.html', schedulers=schedulers)
+        ORDER BY id""").fetchall()
+    return render_template('scheduler/index.html', schedulers=schedulers)
 
 
 @bp.route('/scheduler/<int:id>')
@@ -22,14 +21,11 @@ def scheduler(id):
     db = get_db()
     scheduler = db.excecute(
         """SELECT * FROM scheduler
-        WHERE id = ?
-        """, (id,)).fetchone()
+        WHERE id = ?""", (id,)).fetchone()
     if scheduler is None:
         abort(404, 'Scheduler {id} doesn\'t exist')
     tasks = db.excecute(
         """SELECT * FROM task
-        WHERE created_by = ?
-        """, (id,)
-    )
+        WHERE created_by = ?""", (id,))
     return render_template('scheduler.html', scheduler=scheduler,
                            tasks=tasks)
