6fff1e214629e3c80fd053e879d73c2e41213e5c
Huy-Ngo <duchuy29092000@gmail.com>
2020-04-14 15:33:52 +0700

    Add resetting

--- index.js
+++ index.js
@@ -10,18 +10,31 @@ const app = new Vue({
     data: {
         tasks: stored_tasks,
         name: '',
-        duration: 0
+        duration: 60,
+        last: -1
     },
     methods: {
         add_task: function(event) {
+            for (task of this.tasks)
+                if (task.id > this.last) this.last = task.id
+            console.log(this.last)
             this.tasks.push({
+                id: this.last + 1,
                 name: this.name,
                 total: this.duration,
                 elapsed: 0,
                 status: false
             })
-            localStorage.setItem('rr-scheduler', JSON.stringify(stored_tasks))
+            localStorage.setItem('rr-scheduler', JSON.stringify(this.tasks))
             console.log('Successful.')
+        },
+        reset_all: function(event) {
+            stored_tasks = []
+            this.tasks = []
+            this.last = -1
+            this.duration = 60
+            localStorage.setItem('rr-scheduler', '[]')
+            console.log('Reset everything.')
         }
     }
 });
